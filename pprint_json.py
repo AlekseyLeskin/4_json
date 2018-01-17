f3e7bbbfb93877e92780ef03c282c3099a8dd91f
a.leskin <a.leskin@a.leskin>
2018-01-17 21:50:22 +0300

    fixed variable names - part 2

--- pprint_json.py
+++ pprint_json.py
@@ -3,17 +3,17 @@ import sys
 
 
 def load_data(filepath):
-    with open(filepath, 'r', encoding='utf-8') as f:
-        return json.load(f)
+    with open(filepath, 'r', encoding='utf-8') as open_file:
+        return json.load(open_file)
 
 
-def pretty_print_json(data):
-    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
+def pretty_print_json(json_data):
+    print(json.dumps(json_data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        filepath = load_data(input('Укажите путь до файла: '))
+        user_filepath = load_data(input('Укажите путь до файла: '))
     else:
-        filepath = load_data(sys.argv[1])
-    pretty_print_json(filepath)
+        user_filepath = load_data(sys.argv[1])
+    pretty_print_json(user_filepath)
